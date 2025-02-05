3c7cb26e64c451a37caa9036c1a19ae9e2e76bea
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-02-05 11:19:29 +0100

    JJS

--- js/javascript.js
+++ js/javascript.js
@@ -6,13 +6,26 @@ for (let i = 0; i < myArray.length; i++) {
     console.log(`${i+1}. ${myArray[i]}`);
 }
 
-let person = prompt("Please enter your name", "");
-let text;
-if (person == null || person == "") {
-  text = "User cancelled the prompt.";
+// let person = prompt("Please enter your name", "");
+// let text;
+// if (person == null || person == "") {
+//   text = "User cancelled the prompt.";
+// } else {
+//   alert('Ha en god dag ' + person + '!');
+// } 
+
+
+let ageEntered= prompt("Please enter your age", "");
+if(isNaN(ageEntered)) {
+    alert('Indtast venligst et tal.');
+}
+if (ageEntered <= 17) {
+  alert('Du er ikke gammel nok.');
+} else if (ageEntered >= 18 && ageEntered < 60) {
+    alert('Du er gammel nok.');
 } else {
-  alert('Ha en god dag ' + person + '!');
-} 
+    alert('All inclusive, Tillykke!');
+}
 
 
 
