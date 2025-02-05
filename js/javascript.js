e0444b969024a47a3bd7e0bcf6bd73e3912c0f24
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-02-05 10:57:50 +0100

    Psuh

--- js/javascript.js
+++ js/javascript.js
@@ -6,11 +6,13 @@ for (let i = 0; i < myArray.length; i++) {
     console.log(`${i+1}. ${myArray[i]}`);
 }
 
-let person = prompt("Please enter your name", "Navn");
+let person = prompt("Please enter your name", "");
 let text;
 if (person == null || person == "") {
   text = "User cancelled the prompt.";
 } else {
-  alert('Ha en god dag');
+  alert('Ha en god dag ' + person + '!');
 } 
+
+
 
